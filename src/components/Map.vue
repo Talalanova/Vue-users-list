8584ecac78a002677196b611dabb0e29888cc7cd
Talanova <pro_ego@mail.ru>
2022-05-29 23:49:41 +1000

    add map,markers

---
+++ src/components/Map.vue
@@ -0,0 +1,68 @@
+<template>
+    <section class="map">
+        <div id="mapContainer"></div>
+    </section>
+</template>
+
+<script>
+import L from 'leaflet'  
+import 'leaflet/dist/leaflet.css' 
+import icon from '@/assets/icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+export default {
+    name: "Map",
+    data () {
+        return {
+            centerCoordinates: [-43.9509, -34.4618],
+            map: null,
+            tileLayer: null,
+        }
+    },
+    props: {
+        array: Array,
+        content: String
+    },
+    mounted() {        
+        this.initMap();
+    },  
+    methods: {
+        initMap() {                
+                this.map = L.map('mapContainer')
+                this.map.setView(this.centerCoordinates, 2);                
+                this.tileLayer = L.tileLayer(
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    maxZoom: 18,
+                    id: "mapbox/satellite-v9",                    
+                    }
+                );
+                this.tileLayer.addTo(this.map);
+            },
+        renderMarkers(items) {
+            let markers = L.layerGroup();
+            items.forEach(item => {
+                let lat = item.lat;
+                let lng = item.lng;
+                let marker = L.marker({lat,lng}).bindPopup(this.content);
+                markers.addLayer(marker);
+            });
+            this.map.addLayer(markers);
+        },
+    },
+}
+</script>
+
+<style lang="sass">
+#mapContainer
+    cursor: pointer
+    width: 100%
+    height: 500px
+</style>
--- src/views/User-list.vue
+++ src/views/User-list.vue
@@ -1,74 +1,87 @@
 <template>
-  <section class="users">
-    <div class="users__top-bar">
-      <h1>Список пользователей</h1>
-      <Sorting :arr="users" :city="'city'" :company="'company'"></Sorting>
-    </div>    
-    <Preloader v-if="!usersDownloaded"></Preloader>
-    <Error v-if="error"></Error>
-    <ul class="users__list" v-for="user in users" :key="user.index">
-      <li class="users__item card">
-        <div class="card__text">
-          <p>
-            <span class="card__field">Ф.И.О:</span><span>{{ user.name }}</span>
-          </p>
-          <p>
-            <span class="card__field">Город:</span><span>{{ user.city }}</span>
-          </p>
-          <p>
-            <span class="card__field">Компания:</span><span>{{ user.company }}</span>
-          </p>
-        </div>
-        <router-link class="card__link" :to="'/edit-user/' + user.id">Подробнее</router-link>
-      </li>
-    </ul>
-  </section>
+  <div>
+    <Map ref="map" :array="addresses" :content="user"></Map>
+    <section class="users">
+      <div class="users__top-bar">
+        <h1>Список пользователей</h1>
+        <Sorting :arr="users" :city="'city'" :company="'company'"></Sorting>
+      </div>    
+      <Preloader v-if="!usersDownloaded"></Preloader>
+      <Error v-if="error"></Error>
+      <ul class="users__list" >
+        <li v-for="user in users" :key="user.index" class="users__item card">
+          <div class="card__text">
+            <p>
+              <span class="card__field">Ф.И.О:</span><span>{{ user.name }}</span>
+            </p>
+            <p>
+              <span class="card__field">Город:</span><span>{{ user.address.city }}</span>
+            </p>
+            <p>
+              <span class="card__field">Компания:</span><span>{{ user.company }}</span>
+            </p>
+          </div>
+          <router-link class="card__link" :to="'/edit-user/' + user.id">Подробнее</router-link>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
 import Preloader from '@/components/Preloader.vue'
 import Sorting from '@/components/Sorting.vue'
 import Error from '@/components/Error.vue'
+import Map from '@/components/Map.vue'
 
 export default {
   name: 'Users',
   components: {
     Preloader,
     Sorting,
-    Error
+    Error,
+    Map
   },
   data() {
     return {
       users: [],
       usersDownloaded: false,
-      error: false
+      error: false,
+      addresses: [],
+      user: 'fhdgh'
     }
   },
   methods: {
     loadUsers() {
-      fetch('https://jsonplaceholder.typicode.com/users?offset=0&limit=5')
+      fetch('https://jsonplaceholder.typicode.com/users?offset=0&limit=10')
         .then((response) => {
           if (response.ok) {
             return response.json()
           }
           throw new Error('Network response was not ok');
         })
-        .then((json) => {
-          console.log(json)
+        .then((json) => {          
           this.users = json.map(item => {
             return {
-              name : item.name,
-              city: item.address.city,
+              name : item.name,              
               company: item.company.name,
-              id: item.id
+              id: item.id,
+              address: item.address
             }
           })
           this.usersDownloaded = true
+          this.addresses = this.users.map(item => {
+            return {
+              lat: item.address.geo.lat,
+              lng: item.address.geo.lng
+            }
+          })
+          this.$refs.map.renderMarkers(this.addresses);
         })
         .catch(() => {
           this.error = true
         });
-    }
+    },
   },
   mounted() {
     this.loadUsers();
